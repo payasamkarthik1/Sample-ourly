f85d85427a4cd8527a60b14b99cfaecf1529c24e
vengalavishal250 <vishal@pronteff.com>
2022-11-23 13:34:50 +0530

    added

--- server/services/timeTrackingService.js
+++ server/services/timeTrackingService.js
@@ -887,24 +887,13 @@ function TimeTrackingService(objectCollection) {
                             });
                         });
 
-
-                        console.log('======data1=================')
-                        console.log(data1)
-                        console.log('====================================')
-
                         //renaming object key and calculating total time
                         for (let i = 0; i < data1.length; i++) {
                             data1[i].task_total_time = data1[i].total_hours
                             delete data1[i].total_hour
                         }
-
-                        console.log('=====data1==============');
-                        console.log(data1);
-                        console.log('====================================');
                         let total = await util.calculateWorkedHours(data1)
-                        console.log('========total================')
-                        console.log(total)
-                        console.log('====================================')
+
                         data2[0].total_hour = total
                         data.push(data2[0])
                         responseData = data;
