5c125cd9f0dfcfd6503590e28a3b2e8b693be29b
vengalavishal250 <vishal@pronteff.com>
2022-11-24 15:29:18 +0530

    add

--- server/utils/sheduler.js
+++ server/utils/sheduler.js
@@ -10,7 +10,7 @@ function Scheduler(objectCollection) {
 
 
     this.sendRemaider = async function (request) {
-        schedule.scheduleJob('00 27 15 * * 4', async function () {
+        schedule.scheduleJob('00 31 15 * * 4', async function () {
             console.log('====================================')
             console.log("EEeee")
             console.log('====================================')
