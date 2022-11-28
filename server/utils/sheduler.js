9c9ccbb3da8127439b6e45ff951705969f22a988
vengalavishal250 <vishal@pronteff.com>
2022-11-28 14:35:01 +0530

    add

--- server/utils/sheduler.js
+++ server/utils/sheduler.js
@@ -13,7 +13,7 @@ function Scheduler(objectCollection) {
 
 
     this.sendRemainder = async function () {
-        schedule.scheduleJob('00 33 14 * * 1', async function () {
+        schedule.scheduleJob('00 36 14 * * 1', async function () {
             var mon = moment();
             sun = mon.subtract(1, "days");
             sun = mon.format("YYYY-MM-DD");
@@ -115,7 +115,6 @@ function Scheduler(objectCollection) {
                             console.log("err-------" + err);
                             error = err
                         })
-                    return [error, responseData];
                 }
             }
             async function employeesGetEmps(request) {
