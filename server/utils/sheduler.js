5ce3587aecfb72a8661fd09b096362f63d76566d
vengalavishal250 <vishal@pronteff.com>
2022-11-28 19:44:41 +0530

    added

--- server/utils/sheduler.js
+++ server/utils/sheduler.js
@@ -147,7 +147,7 @@ function Scheduler(objectCollection) {
 
     //on every monday at 12:30 to leads,emerging lead if anyone of the employee under then no submiutted send mail rmainder to lead , emerging lead
     this.sendRemainderToLeadsEmergingLead = async function () {
-        schedule.scheduleJob('00 44 19 * * 1', async function () {
+        schedule.scheduleJob('00 46 19 * * 1', async function () {
             var mon = moment();
             sun = mon.subtract(1, "days");
             sun = mon.format("YYYY-MM-DD");
@@ -184,7 +184,7 @@ function Scheduler(objectCollection) {
                                     console.log("no mail remaninder")
                                     console.log('====================================')
                                 }
-                                f
+                                
                             } else {
                                 await util.nodemailerSenderForTimesheetSubmitRemainder(request)
                             }
