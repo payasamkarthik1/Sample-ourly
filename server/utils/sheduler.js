040c871bf594241ae0ac3aa7d0d5601b84a95a86
vengalavishal250 <vishal@pronteff.com>
2022-11-28 19:42:35 +0530

    aded

--- server/utils/sheduler.js
+++ server/utils/sheduler.js
@@ -147,7 +147,7 @@ function Scheduler(objectCollection) {
 
     //on every monday at 12:30 to leads,emerging lead if anyone of the employee under then no submiutted send mail rmainder to lead , emerging lead
     this.sendRemainderToLeadsEmergingLead = async function () {
-        schedule.scheduleJob('00 40 19 * * 1', async function () {
+        schedule.scheduleJob('00 44 19 * * 1', async function () {
             var mon = moment();
             sun = mon.subtract(1, "days");
             sun = mon.format("YYYY-MM-DD");
@@ -203,7 +203,7 @@ function Scheduler(objectCollection) {
                             if (empsSubmitted.length != 0) {
                                 //comapring  empsSubmitted with lead emps
                                 const arrayTwoEmails = new Set(empsSubmitted.map((el) => el.email));
-                                const arrayOneFiltered = leadEmps.filter((el) => arrayTwoEmails.has(el.email));
+                                const arrayOneFiltered = emrgingEmps.filter((el) => arrayTwoEmails.has(el.email));
                                 Array.prototype.push.apply(filterEmergingEmpsApproved, arrayOneFiltered);
                                 if (filterEmergingEmpsApproved.length == 0) {
                                     await util.nodemailerSenderForTimesheetSubmitRemainder(request)
