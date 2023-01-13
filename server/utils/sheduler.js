294244955cd6b40a264cf629d8b720b7ed7c3f30
vengalavishal250 <vishal@pronteff.com>
2023-01-13 11:47:19 +0530

    a

--- server/utils/sheduler.js
+++ server/utils/sheduler.js
@@ -17,7 +17,7 @@ function Scheduler(objectCollection) {
     //on every monday at 10:30 to leads,emerging lead,users considering as all individual
     this.sendRemainder = async function () {
         console.log("-------------------------entered sendRemainder------------------------------");
-        schedule.scheduleJob('00 39 11 * * 5', async function () {
+        schedule.scheduleJob('00 49 11 * * 5', async function () {
             var mon = moment();
             sun = mon.subtract(5, "days");
             sun = mon.format("YYYY-MM-DD");
@@ -51,7 +51,7 @@ function Scheduler(objectCollection) {
                     sendMails = emps
                 }
                 console.log('===========sendMails=================')
-                 console.log(sendMails.email)
+                 console.log(sendMails)
                 console.log('====================================')
                 await sendMails.map(async (mail) => {
                     request.email = mail.email
