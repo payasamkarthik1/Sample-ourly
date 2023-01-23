72c752a1138c7928e115637380ccf71b12dcfa24
vengalavishal250 <vishal@pronteff.com>
2023-01-23 12:14:00 +0530

    a

--- server/utils/sheduler.js
+++ server/utils/sheduler.js
@@ -91,7 +91,7 @@ function Scheduler(objectCollection) {
     //on every monday at 12:30 send mail to lead if any emp is not submitted to lead
     this.sendRemainderToLeads = async function () {
         console.log("-------------------------entered sendRemainder1------------------------------");
-        schedule.scheduleJob('00 30 12 * * 1', async function () {
+        schedule.scheduleJob('00 30 10* * 1', async function () {
             var mon = moment();
             sun = mon.subtract(1, "days");
             sun = mon.format("YYYY-MM-DD");
