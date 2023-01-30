1eca2323ec24632ff65ab2e8fc0f6b44af1037dd
vengalavishal250 <vishal@pronteff.com>
2023-01-30 16:13:51 +0530

    added

--- server/utils/util.js
+++ server/utils/util.js
@@ -163,7 +163,7 @@ function Util() {
                                                     <tr>
                                                         <td align="left"
                                                             style="font-family:Arial, sans-serif; font-size: 16px;padding: 12px 0;">
-                                                            <a href="https://192.168.0.251:4179/forgotpass">
+                                                            <a href="http://183.82.113.10:4179/forgotpass">
                                                             <button
                                                                 style="background: #d46706;color: #ffffff;border: 0;padding: 10px 50px;cursor: pointer;">Click to Change Password</button>
                                                                 </a>
