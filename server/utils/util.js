e18322538fe3ffd462f87fd1b6cb0975ff61af4b
vengalavishal250 <vishal@pronteff.com>
2023-01-31 14:30:28 +0530

    added staging

--- server/utils/util.js
+++ server/utils/util.js
@@ -285,7 +285,7 @@ function Util() {
                                                     <tr>
                                                         <td align="left"
                                                             style="font-family:Arial, sans-serif; font-size: 16px;padding: 12px 0;">
-                                                            <a href="https://www.ourly.pronteff.com/">
+                                                            <a href="http://183.82.113.10:4179">
                                                             <button
                                                                 style="background: #d46706;color: #ffffff;border: 0;padding: 10px 50px;cursor: pointer;">Submit Your Timesheet</button>
                                                                 </a>
@@ -420,7 +420,7 @@ function Util() {
                                                 <tr>
                                                 <td align="left"
                                                     style="font-family:Arial, sans-serif; font-size: 16px;padding: 12px 0;">
-                                                    <a href="https://www.ourly.pronteff.com/">
+                                                    <a href="http://183.82.113.10:4179">
                                                     <button
                                                         style="background: #d46706;color: #ffffff;border: 0;padding: 10px 50px;cursor: pointer;">Review</button>
                                                         </a>
@@ -551,7 +551,7 @@ function Util() {
                             <tr>
                                                         <td align="left"
                                                             style="font-family:Arial, sans-serif; font-size: 16px;padding: 12px 0;">
-                                                            <a href="https://www.ourly.pronteff.com/">
+                                                            <a href="http://183.82.113.10:4179">
                                                             <button
                                                                 style="background: #d46706;color: #ffffff;border: 0;padding: 10px 50px;cursor: pointer;">Review</button>
                                                                 </a>
@@ -796,7 +796,7 @@ function Util() {
                             <tr>
                             <td align="left"
                                 style="font-family:Arial, sans-serif; font-size: 16px;padding: 12px 0;">
-                                <a href="https://www.ourly.pronteff.com/">
+                                <a href="http://183.82.113.10:4179">
                                 <button
                                     style="background: #d46706;color: #ffffff;border: 0;padding: 10px 50px;cursor: pointer;">Review</button>
                                     </a>
