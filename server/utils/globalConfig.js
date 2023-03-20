8a911fb6e3642bbf8de6ad95e96b6ead1fe4630f
vengalavishal111 <vishalvengala7@gmail.com>
2023-03-20 18:01:49 +0530

    aded

--- server/utils/globalConfig.js
+++ server/utils/globalConfig.js
@@ -8,9 +8,9 @@ mode = "staging";
 config = {};
 
 if (mode === "staging") {
-    config.user = "mydb",
+    config.user = "root",
         config.host = "localhost",
-        config.password = "Passw0rd!",
+        config.password = "Welcome@1234",
         config.database = "clockify",
         config.multipleStatements = true,
         config.sceret_key = "clockify"
