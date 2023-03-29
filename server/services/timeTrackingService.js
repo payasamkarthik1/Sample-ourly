5b9dc02d2980d02afd5cac16cd11a81383d1c533
vengalavishal111 <vishalvengala7@gmail.com>
2023-03-29 12:08:33 +0530

    added

--- server/services/timeTrackingService.js
+++ server/services/timeTrackingService.js
@@ -1121,7 +1121,7 @@ function TimeTrackingService(objectCollection) {
                             console.log('=============data222222222222================');
                             console.log(data2);
                             console.log('====================================');
-                            data2[j].push({ submitted_datetime: data[0].submited_for_approval_datetime })
+                            data2[j].submitted_datetime = data[0].submited_for_approval_datetime
                         } else if (data2[j].status_id == 3) {
                             console.log("entered    3");
 
@@ -1129,8 +1129,8 @@ function TimeTrackingService(objectCollection) {
                             request.first_week_day = data2[j].first_week_day
                             request.last_week_day = data2[j].last_week_day
                             const [error, data] = await this.getSubmittedApproveEntries(request, 2)
-                            data2[j].push({ approved_datetime: data[0].approved_on_datetime })
-                            data2[j].push({ approved_by: data[0].approved_by })
+                            data2[j].approved_datetime = data[0].approved_on_datetime
+                            data2[j].approved_by = data[0].approved_by
 
                         }
                         Array.prototype.push.apply(responseData, data2);
