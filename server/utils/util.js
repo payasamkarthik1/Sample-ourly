7df79d324bf314783f12697088e9b6e4a3b1cd5d
vengalavishal111 <vishalvengala7@gmail.com>
2023-04-05 15:01:48 +0530

    added btn

--- server/utils/util.js
+++ server/utils/util.js
@@ -807,15 +807,15 @@ function Util() {
                                 </td>
                             </tr>
                             
-                            //      <tr>
-                            //      <td align="left"
-                            //          style="font-family:Arial, sans-serif; font-size: 16px;padding: 12px 0;">
-                            //          <a href="http://183.82.113.10:4179/approvals">
-                            //      <button
-                            //              style="background: #d46706;color: #ffffff;border: 0;padding: 10px 50px;cursor: pointer;">Review</button>
-                            //              </a>
-                            //      </td>
-                            //  </tr>
+                                 <tr>
+                                  <td align="left"
+                                      style="font-family:Arial, sans-serif; font-size: 16px;padding: 12px 0;">
+                                      <a href="http://183.82.113.10:4179/approvals">
+                                  <button
+                                          style="background: #d46706;color: #ffffff;border: 0;padding: 10px 50px;cursor: pointer;">Review</button>
+                                          </a>
+                                  </td>
+                              </tr>
                             <tr>
                                 <td
                                     style="border-top:1px solid #ccc; font-size:12px; font-family: Arial, sans-serif;text-align: center;">
