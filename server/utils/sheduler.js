e5ae1c1e84918ab17a264f4b1d7b57d4f6ecf37d
bhavanaguptha <81694211+bhavanaguptha@users.noreply.github.com>
2023-07-05 15:11:13 +0530

    u

--- server/utils/sheduler.js
+++ server/utils/sheduler.js
@@ -174,7 +174,7 @@ function Scheduler(objectCollection) {
 
     this.sendRemainderToProjectLeads = async function () {
         console.log("-------------------------entered sendRemainderToProjetLeads------------------------------");
-        schedule.scheduleJob('40 15 * * *', async function () {
+        schedule.scheduleJob('25 15 * * *', async function () {
             const [err, res, weekName] = await leadService.getProjectsAndLeads()
 
             for (let item of res) {
@@ -376,7 +376,7 @@ function Scheduler(objectCollection) {
 
     this.sendSubmittedAndUnsubmittedListToLeads = async function () {
         console.log("-------------------------entered sendSubmittedAndUnsubmittedListToLeads------------------------------");
-        schedule.scheduleJob('42 15 * * *', async function () {
+        schedule.scheduleJob('30 15 * * *', async function () {
             const [err, res, weekName] = await leadService.getLeadProjectsAndSelfApprovalDataForEmailSending()
 
             for (let item of res) {
