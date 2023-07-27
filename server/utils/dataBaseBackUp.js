c953992ed4ca1e97be7e6a6eea32a165d61ce95c
bhavanaguptha <81694211+bhavanaguptha@users.noreply.github.com>
2023-07-27 18:32:03 +0530

    updates

--- server/utils/dataBaseBackUp.js
+++ server/utils/dataBaseBackUp.js
@@ -6,25 +6,6 @@ const checkserver = async(req)=>{
   //console.log("check server",req.nodeip);
  return await new Promise((myReslove,myReject)=>{
     var v_host = 'XX.XX.XX.XXX'
-    // let kk = exec('E:/codeBase/pronteff_clockify/Ourly_backend/Clockify_backend/server/utils/backup_db.sh',async(error,stderr,stdout)=>{
-    //   console.log("ssssss",stdout);
-    //   console.log("nnnnn",stderr);
-    //   console.log("eeee",error);
-    //   await setTimeout(() => console.log('Timeout'), 10000);
-      
-    //   myReslove(rerddata)
-    //   if(rerddata.length>0){
-    //    myReslove(rerddata)
-    //   }
-    //   if(stderr.length>0){
-    //    myReslove(0)
-    //   }
-    //   else{C:/Users/SSC/Documents/dumps/ourly.sql
-
-    //    myReslove(rerddata) /home/ourly/Desktop/databasebackup
-    //   } 'mysqldump --host ourlyproddatabase.ctthlmqttiri.ap-south-1.rds.amazonaws.com --user ourly_master --password=Our1y!MYSQL* --port 1525 --single-transaction --routines --set-gtid-purged=OFF --triggers ourly > /home/ourly/Desktop/databasebackup/ourly.sql'   /ourly/dbbackup
-      
-    // })
     let kk = exec('mysqldump --host ' + process.env.aws_host + ' --user ' + process.env.aws_user + ' --password='+process.env.aws_password + ' --port ' + process.env.AWS_PORT + ' --single-transaction --routines --set-gtid-purged=OFF --triggers ' + process.env.aws_database +' > /ourly/dbbackup/ourly.sql',
       function (error, stdout, stderr) {
         console.log('stdout: ' + stdout);
@@ -38,7 +19,6 @@ const checkserver = async(req)=>{
 
   })
 }
-//console.log('mysqldump --host ' + process.env.aws_host + ' --user ' + process.env.aws_user + ' --password='+process.env.aws_password + ' --port ' + process.env.AWS_PORT + ' --single-transaction --routines --set-gtid-purged=OFF --triggers ' + process.env.aws_database +' > C:/Users/SSC/Documents/dumps/ourly.sql');
 
 //checkserver()
 
